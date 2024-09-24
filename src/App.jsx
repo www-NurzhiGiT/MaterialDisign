446487596b8c4347999fbed3b3ce1df1c86bfec7
Nurzhigit <Nurjigit.zwer@gmail.com>
2024-09-24 09:50:39 +0600

    map array

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const cards = [1, 2, 3, 4, 5, 6, 7];
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 function App() {
   const classes = useStyles();
   return (
